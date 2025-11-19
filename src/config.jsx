788829c8e04b3c9c8553e01cc62e3e08c6a8912a
momin0933 <122223850+momin0933@users.noreply.github.com>
2025-11-19 12:27:48 +0600

    Update config.jsx

--- src/config.jsx
+++ src/config.jsx
@@ -1,6 +1,6 @@
 const config ={
-    API_BASE_URL:"https://localhost:44352/",
- //API_BASE_URL:"https://api.minglefashionbd.com/",  
+    //API_BASE_URL:"https://localhost:44352/",
+ API_BASE_URL:"https://api.minglefashionbd.com/",  
  tenantId:"Exam"   
 // tenantId:"FtexExam"  
    };
